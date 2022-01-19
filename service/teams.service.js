91841e18f5f707eae0c10ef0ce9502e7883b3ed0
ron merkin <ron.merkin@nielsen.com>
2022-01-19 21:15:40 +0200

    add settimeout

--- service/teams.service.js
+++ service/teams.service.js
@@ -7,6 +7,8 @@ const getTeams = async (team = undefined) => {
         if (!teams || !teams.length) {
             throw new Error('No team Found')
         }
+        // add settimeout in order to be able to display proper loading on frontend side
+        await new Promise(resolve => setTimeout(resolve, 3000))
         return teams
     } catch (err) {
         throw err
